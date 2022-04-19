3f8171f3eeeef81c27214017b86b9a8c8f5b79a5
Giuseppe Morana <giuseppe.morana@deliveroo.co.uk>
2022-04-19 07:57:15 +0200

    RPE-1055: Bitrise Step GitHub script runner (#1)
    * Add step inputs; Add test workflows
    
    * Use a file in the repo to test the upload
    
    * Remove todos
    
    * Add new input: tmp_folder
    
    * Add codeowners and pr template
    
    * Make Bitrise point to master

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,17 @@
+## :loudspeaker: Type of change
+- [ ] Bugfix
+- [ ] New feature
+- [ ] Enhancement
+- [ ] Refactoring
+- [ ] Cleanup
+
+## :writing_hand: Ticket
+<!-- XXX-123 (please insert correct ticket number and Jira automation will link to it automatically) -->
+
+## :scroll: Description
+<!-- Please describe your changes to help the reviewer. -->
+
+## :test_tube: How did you test it?
+- [ ] Tests already present
+- [ ] Added automated tests
+- [ ] Verified manually <!-- (Please specify which test cases you have roughly tried. This will be useful for the reviewer in order to suggest edge cases that may have been missed.) -->
--- step.sh
+++ step.sh
@@ -1,22 +1,60 @@
 #!/bin/bash
-set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
-
+# Downloads the content of a script hosted in GitHub and runs it as a bash script.
+# It is very similar to Remote Bash Script Runner Step but you can also:
 #
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+#   1. Use it with private repos
+#   2. Pass command line arguments
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+set -e
+
+function echoerr() { echo "$@" 1>&2; }
+
+# --- Input validation ---
+
+if [ -z $repository ]; then
+    echoerr "Missing \$repository env var"
+    exit 1
+fi
+
+if [ -z $script_path ]; then
+    echoerr "Missing \$script_path env var"
+    exit 1
+fi
+
+if [ -z $branch ]; then
+    echoerr "Missing \$branch env var"
+    exit 1
+fi
+
+if [ -z $github_token ]; then
+    echoerr "Missing \$github_token env var"
+    exit 1
+fi
+
+if [ -z $tmp_folder ]; then
+    echoerr "Missing \$tmp_folder env var"
+    exit 1
+fi
+
+echo "repository = $repository"
+echo "script_path = $script_path"
+echo "branch = $branch"
+echo "command_line_arguments = $command_line_arguments"
+echo "tmp_folder = $tmp_folder"
+
+mkdir -p $tmp_folder
+
+echo "Downloading the remote script locally..."
+
+downloaded_script_path="$tmp_folder/remote-script.sh"
+
+curl -H "Authorization: token $GITHUB_TOKEN" \
+  -H 'Accept: application/vnd.github.v3.raw' \
+  -sSL "https://api.github.com/repos/$repository/contents/$script_path?ref=$branch" &> $downloaded_script_path
+chmod +x $downloaded_script_path
+
+echo "Running the remote script..."
+
+command="$downloaded_script_path $command_line_arguments"
+bash -c "$command"
